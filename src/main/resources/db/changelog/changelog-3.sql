2cd1fcb4ddec0677857f4e3175d68cbea783bd27
TatsianaKobets <kobets.tatsiana@gmail.com>
2024-12-24 12:27:05 +0300

    Security block refactoring

--- src/main/resources/db/changelog/changelog-3.sql
+++ src/main/resources/db/changelog/changelog-3.sql
@@ -1,15 +1,12 @@
 --liquibase formatted sql
 
 --changeset TatsianaKobets:3
---comment first migration
+--comment create users table
 create table users
 (
     id       serial primary key,
     username varchar(255) not null,
     password varchar(255) not null
 );
-
-insert into users (username, password)
-values ('admin', 'password');
 
 --rollback drop table users;
