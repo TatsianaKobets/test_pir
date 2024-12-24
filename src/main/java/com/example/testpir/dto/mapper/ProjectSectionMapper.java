8c7c5fec5230d8714424b1ca636f5c11fbbafce5
TatsianaKobets <kobets.tatsiana@gmail.com>
2024-12-24 13:00:19 +0300

    Refactoring

--- src/main/java/com/example/testpir/dto/mapper/ProjectSectionMapper.java
+++ src/main/java/com/example/testpir/dto/mapper/ProjectSectionMapper.java
@@ -9,16 +9,16 @@ public class ProjectSectionMapper {
   public static ProjectSectionDTO toDto(ProjectSection projectSection) {
     ProjectSectionDTO dto = new ProjectSectionDTO();
     dto.setId(projectSection.getId());
-    dto.setSection(projectSection.getName()); // Изменено на getName()
-    dto.setType(projectSection.getType().name()); // Добавлено для типа
+    dto.setSection(projectSection.getName());
+    dto.setType(projectSection.getType().name());
     return dto;
   }
 
   public static ProjectSection toEntity(ProjectSectionDTO dto) {
     ProjectSection projectSection = new ProjectSection();
     projectSection.setId(dto.getId());
-    projectSection.setName(dto.getSection()); // Изменено на setName()
-    projectSection.setType(SectionType.valueOf(dto.getType())); // Добавлено для типа
+    projectSection.setName(dto.getSection());
+    projectSection.setType(SectionType.valueOf(dto.getType()));
     return projectSection;
   }
 }
